299f9688f3a87707e87645f4386bf194f75d1bc5
memelopez <meme_lopez10@hotmail.com>
2015-12-10 23:55:43 -0600

    Made changes

--- Campo.h
+++ Campo.h
@@ -1,43 +1,42 @@
-/*
-Esta es la calse que representa un campo de la estructura
-esta clase campo solo es un tipo de concha donde se ponen los datos de verdad, es como un envase para los datos 
-Elmer Lopez
-*/
-#pragma once
+#ifndef CAMPO_H
+#define CAMPO_H
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <cstdlib>
+#include <QString>
+#include <QFile>
 
-using namespace std; 
+using namespace std;
 
-class Campo {
-		char name[30];
-		int fieldtype;//0-texto 1-entero 2-decimal 3-ID
-		int size;
-		int sizedecimal;
-		int keytype;//0- nada 1- llave primiaria 2- llave secundaria 
+class Campo{
+    char name[30];
+    int fieldtype;//0-texto 1-entero 2-decimal 3-ID
+    int size;
+    int sizedecimal;
+    int keytype;//0- nada 1- llave primiaria 2- llave secundaria
 
-	public:
-		Campo();
-		Campo(const char*, int, int, int, int);
-		string toString()const;
-		string inttoString(int, bool) const;
-		string toStringArchivo() const; 
-		friend istream& operator>>(istream&, Campo&);
-		friend ofstream& operator << (ofstream&, const Campo&);
-		friend ifstream& operator >> (ifstream&, Campo&);
+public:
+    Campo();
+    Campo(const char*, int, int, int, int);
+    string toString()const;
+    string inttoString(int, bool) const;
+    string toStringArchivo() const;
+    friend istream& operator>>(istream&, Campo&);
+    friend ofstream& operator << (ofstream&, const Campo&);
+    friend ifstream& operator >> (ifstream&, Campo&);
 
-		void setFieldtype(int);
-		int getFieldtype()const;
-		void setName(const char*);
-		const char* getName()const;
-		void setSize(int);
-		int getSize()const;
-		void setSizedecimal(int);
-		int getSizedecimal()const;
-		void setKeytype(int);
-		int getKeytype()const;
+    void setFieldtype(int);
+    int getFieldtype()const;
+    void setName(const char*);
+    const char* getName()const;
+    void setSize(int);
+    int getSize()const;
+    void setSizedecimal(int);
+    int getSizedecimal()const;
+    void setKeytype(int);
+    int getKeytype()const;
 
-		//podriamos poner gets y sets para cambiar cosas acerca de los campos 
+    //podriamos poner gets y sets para cambiar cosas acerca de los campos
 };
+#endif // CAMPO_H
